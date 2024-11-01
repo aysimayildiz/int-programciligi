c67417ffe93745598081c7d68b6854dd5f9bcac6
aysimayildiz <aysima.yildiz@ogr.gelisim.edu.tr>
2024-11-01 11:02:10 +0300

    Add files via upload

---
+++ hesapla.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($_POST["kaydet"]))
+{
+    $adi=$_POST["adi"];
+    $soyadi=$_POST["soyadi"];
+    $vize=$_POST["vize"];
+    $final=$_POST["final"];
+    $proje=$_POST["proje"];
+    $ortalama=($vize*0.5)+($final*0.25)+($final*0.25);
+    ($proje*0.25);
+    echo " merhaba ".$adi." ".$soyadi." <br>"."Vize notun:".$vize."<br>"."Final notun:".$final."<br>"."proje notun:".$proje."<br>"."ortalaman:".$ortalama."<br>";
+    if($ortalama>40 && $proje>=40)
+    {
+        echo "tebrikler ".$ortalama."ortalama ile dersi tamamladın";
+
+    }
+    else
+    {
+        echo"malesef ".$ortalama."ile dersten kaldın.";
+    }
+}
+?>
