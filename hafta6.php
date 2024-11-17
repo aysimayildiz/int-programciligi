242eba82cf7354dda9f899dbd52f28f18cc9dcaa
aysimayildiz <aysima.yildiz@ogr.gelisim.edu.tr>
2024-11-17 18:35:16 +0300

    hafta6.php

--- hafta6.php
+++ hafta6.php
@@ -79,3 +79,20 @@ for ($i=0;$i<=10;$i++)
 }
 */
 ?>
+
+
+$toplam=0;
+echo "1 ve 100 arasındaki sayılardan hem 2'ye hem de 5'e bölünen <br> sayıları listeleme ve toplam sonucunu bulalım."."<br>";
+for($i=1; $i<=100;$i++)
+{
+    
+    if ($i%2==0 && $i%5==0)
+     {
+        echo $i."<br>";
+        $toplam=$toplam+$i;
+     }
+}
+echo "<b>Toplam sonucu: </b>".$toplam;
+?>
+
+
